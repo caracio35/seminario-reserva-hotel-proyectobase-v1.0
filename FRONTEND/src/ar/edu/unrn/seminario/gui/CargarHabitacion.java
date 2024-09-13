438ecf6e0f0e83f94d8ec1447985dd884665e52b
andressssm <andressanmartin894@gmail.com>
2024-09-13 19:20:11 -0300

    Creacion de Ventanas
    cree las ventanas Cargar Habitacion y Confirmar Reserva , es solo estetico sin funcionalidades

---
+++ FRONTEND/src/ar/edu/unrn/seminario/gui/CargarHabitacion.java
@@ -0,0 +1,78 @@
+package ar.edu.unrn.seminario.gui;
+
+import java.awt.TextField;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class CargarHabitacion extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	public CargarHabitacion() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+
+		JPanel panel = new JPanel();
+		panel.setBounds(10, 10, 416, 243);
+		contentPane.add(panel);
+		panel.setLayout(null);
+
+		TextField textFieldNumeroHabitacion = new TextField();
+		textFieldNumeroHabitacion.setBounds(10, 39, 150, 21);
+		panel.add(textFieldNumeroHabitacion);
+
+		TextField textFieldCamas = new TextField();
+		textFieldCamas.setBounds(243, 39, 150, 21);
+		panel.add(textFieldCamas);
+
+		TextField textFieldDescripccion = new TextField();
+		textFieldDescripccion.setBounds(120, 87, 150, 21);
+		panel.add(textFieldDescripccion);
+
+		TextField textFieldPrecioRegistrado = new TextField();
+		textFieldPrecioRegistrado.setBounds(243, 172, 150, 21);
+		panel.add(textFieldPrecioRegistrado);
+
+		TextField textFieldPrecioNoRegistrado = new TextField();
+		textFieldPrecioNoRegistrado.setBounds(10, 172, 150, 21);
+		panel.add(textFieldPrecioNoRegistrado);
+
+		JButton btnSubirInformacion = new JButton("Subir Informacion");
+		btnSubirInformacion.setBounds(126, 222, 144, 21);
+		panel.add(btnSubirInformacion);
+
+		JButton btnCargarImagen = new JButton("Cargar Imagen");
+		btnCargarImagen.setBounds(0, 222, 116, 21);
+		panel.add(btnCargarImagen);
+
+		JLabel lblNumeroHabitacion = new JLabel("Numero Habitacion");
+		lblNumeroHabitacion.setBounds(10, 20, 130, 13);
+		panel.add(lblNumeroHabitacion);
+
+		JLabel lblCamas = new JLabel("Camas");
+		lblCamas.setBounds(243, 20, 45, 13);
+		panel.add(lblCamas);
+
+		JLabel lblDescripcion = new JLabel("Descripccion");
+		lblDescripcion.setBounds(120, 68, 107, 13);
+		panel.add(lblDescripcion);
+
+		JLabel lblPrecioRegistrado = new JLabel("Precio Registrado");
+		lblPrecioRegistrado.setBounds(10, 153, 106, 13);
+		panel.add(lblPrecioRegistrado);
+
+		JLabel lblPrecioNoRegistrado = new JLabel("Precio No Registrado");
+		lblPrecioNoRegistrado.setBounds(243, 153, 107, 13);
+		panel.add(lblPrecioNoRegistrado);
+	}
+}
---
+++ FRONTEND/src/ar/edu/unrn/seminario/gui/ConfirmarReserva.java
@@ -0,0 +1,119 @@
+package ar.edu.unrn.seminario.gui;
+
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
+
+public class ConfirmarReserva extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	private JTextField textFieldNombre;
+	private JTextField textFieldApellido;
+	private JTextField textFieldDNIPasaporte;
+	private JTextField textFieldFechaIngreso;
+	private JTextField textFieldFechaSalida;
+	private JTable table;
+	private final ButtonGroup buttonGroup = new ButtonGroup();
+
+	public ConfirmarReserva() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 455);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+
+		JPanel panel = new JPanel();
+		panel.setBounds(10, 10, 416, 398);
+		contentPane.add(panel);
+		panel.setLayout(null);
+
+		textFieldNombre = new JTextField();
+		textFieldNombre.setBounds(10, 57, 96, 19);
+		panel.add(textFieldNombre);
+		textFieldNombre.setColumns(10);
+
+		textFieldApellido = new JTextField();
+		textFieldApellido.setBounds(10, 101, 96, 19);
+		panel.add(textFieldApellido);
+		textFieldApellido.setColumns(10);
+
+		textFieldDNIPasaporte = new JTextField();
+		textFieldDNIPasaporte.setBounds(10, 146, 96, 19);
+		panel.add(textFieldDNIPasaporte);
+		textFieldDNIPasaporte.setColumns(10);
+
+		JComboBox comboBoxMetodoPago = new JComboBox();
+		comboBoxMetodoPago.setBounds(10, 200, 96, 21);
+		panel.add(comboBoxMetodoPago);
+
+		JRadioButton rdbtnPrecioMinimo = new JRadioButton("Precio Minimo $");
+		buttonGroup.add(rdbtnPrecioMinimo);
+		rdbtnPrecioMinimo.setBounds(0, 331, 103, 21);
+		panel.add(rdbtnPrecioMinimo);
+
+		JRadioButton rdbtnPagoTotal = new JRadioButton("Pago Total $");
+		buttonGroup.add(rdbtnPagoTotal);
+		rdbtnPagoTotal.setBounds(140, 331, 103, 21);
+		panel.add(rdbtnPagoTotal);
+
+		textFieldFechaIngreso = new JTextField();
+		textFieldFechaIngreso.setBounds(10, 255, 96, 19);
+		panel.add(textFieldFechaIngreso);
+		textFieldFechaIngreso.setColumns(10);
+
+		textFieldFechaSalida = new JTextField();
+		textFieldFechaSalida.setBounds(10, 306, 96, 19);
+		panel.add(textFieldFechaSalida);
+		textFieldFechaSalida.setColumns(10);
+
+		JButton btnRealizarPago = new JButton("Realizar Pago");
+		btnRealizarPago.setBounds(289, 367, 117, 21);
+		panel.add(btnRealizarPago);
+
+		JButton btnCancelar = new JButton("Cancelar");
+		btnCancelar.setBounds(194, 367, 85, 21);
+		panel.add(btnCancelar);
+
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(140, 160, 266, 154);
+		panel.add(scrollPane);
+
+		table = new JTable();
+		scrollPane.setViewportView(table);
+
+		JLabel lblNombre = new JLabel("Nombre");
+		lblNombre.setBounds(10, 34, 45, 13);
+		panel.add(lblNombre);
+
+		JLabel lblApellido = new JLabel("Apellido");
+		lblApellido.setBounds(10, 86, 45, 13);
+		panel.add(lblApellido);
+
+		JLabel lblDNIPasaporte = new JLabel("DNI/Pasaporte");
+		lblDNIPasaporte.setBounds(10, 130, 45, 13);
+		panel.add(lblDNIPasaporte);
+
+		JLabel lblMetodoDePago = new JLabel("Metodo de Pago");
+		lblMetodoDePago.setBounds(10, 177, 96, 13);
+		panel.add(lblMetodoDePago);
+
+		JLabel lblFechaIngreso = new JLabel("Fecha Ingreso");
+		lblFechaIngreso.setBounds(10, 231, 96, 13);
+		panel.add(lblFechaIngreso);
+
+		JLabel lblFechaSalida = new JLabel("Fecha Salida");
+		lblFechaSalida.setBounds(10, 284, 96, 13);
+		panel.add(lblFechaSalida);
+	}
+}
