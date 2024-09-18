fa26815cb68a265637f609a04854f8d831710a8b
Bruno20H <ricardohuaiky@gmail.com>
2024-09-17 23:06:09 -0300

    Ventana Realizar Pago

--- FRONTEND/src/ar/edu/unrn/seminario/gui/PagarReserva.java
+++ FRONTEND/src/ar/edu/unrn/seminario/gui/PagarReserva.java
@@ -20,84 +20,86 @@ public class PagarReserva extends JFrame {
 	private JTextField textField_1;
 	private JTextField textField_2;
 	private JTextField textField_3;
+	private JTextField textField_4;
 	
 	public PagarReserva() {
 		setTitle("Realizar Pago");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 474);
+		setBounds(100, 100, 378, 423);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JRadioButton rdbtnNewRadioButton = new JRadioButton("Usuario Registrado");
-		rdbtnNewRadioButton.setBounds(23, 37, 140, 23);
-		contentPane.add(rdbtnNewRadioButton);
-		
-		JRadioButton rdbtnNewRadioButton_1 = new JRadioButton("Usuario No Registrado");
-		rdbtnNewRadioButton_1.setBounds(165, 37, 156, 23);
-		contentPane.add(rdbtnNewRadioButton_1);
-		
-		JLabel lblNewLabel = new JLabel("Metodo de Pago ");
-		lblNewLabel.setBounds(23, 84, 89, 14);
+		JLabel lblNewLabel = new JLabel("Metodo De Pago ");
+		lblNewLabel.setBounds(23, 29, 89, 14);
 		contentPane.add(lblNewLabel);
 		
 		JComboBox comboBox = new JComboBox();
 		comboBox.setToolTipText("");
-		comboBox.setBounds(23, 103, 161, 22);
+		comboBox.setBounds(23, 43, 161, 22);
 		contentPane.add(comboBox);
 		
 		textField = new JTextField();
-		textField.setBounds(23, 150, 181, 20);
+		textField.setBounds(23, 91, 181, 20);
 		contentPane.add(textField);
 		textField.setColumns(10);
 		
 		JLabel lblNewLabel_1 = new JLabel("Numero De Tarjeta");
-		lblNewLabel_1.setBounds(23, 136, 119, 14);
+		lblNewLabel_1.setBounds(23, 76, 119, 14);
 		contentPane.add(lblNewLabel_1);
 		
 		textField_1 = new JTextField();
-		textField_1.setBounds(23, 197, 181, 20);
+		textField_1.setBounds(23, 136, 181, 20);
 		contentPane.add(textField_1);
 		textField_1.setColumns(10);
 		
-		JLabel lblNewLabel_2 = new JLabel("Nombre De Tarjeta");
-		lblNewLabel_2.setBounds(23, 181, 129, 14);
+		JLabel lblNewLabel_2 = new JLabel("Nombre Del Titular de la Tarjeta");
+		lblNewLabel_2.setBounds(23, 122, 181, 14);
 		contentPane.add(lblNewLabel_2);
 		
 		textField_2 = new JTextField();
-		textField_2.setBounds(229, 150, 119, 20);
+		textField_2.setBounds(229, 91, 119, 20);
 		contentPane.add(textField_2);
 		textField_2.setColumns(10);
 		
 		JLabel lblNewLabel_3 = new JLabel("Fecha Vencimiento ");
-		lblNewLabel_3.setBounds(229, 136, 119, 14);
+		lblNewLabel_3.setBounds(229, 76, 119, 14);
 		contentPane.add(lblNewLabel_3);
 		
 		textField_3 = new JTextField();
-		textField_3.setBounds(229, 197, 86, 20);
+		textField_3.setBounds(229, 136, 86, 20);
 		contentPane.add(textField_3);
 		textField_3.setColumns(10);
 		
 		JLabel lblNewLabel_4 = new JLabel("CVV");
-		lblNewLabel_4.setBounds(229, 181, 46, 14);
+		lblNewLabel_4.setBounds(229, 122, 46, 14);
 		contentPane.add(lblNewLabel_4);
 		
 		JButton btnNewButton = new JButton("Confirmar Pago");
-		btnNewButton.setBounds(314, 407, 107, 23);
+		btnNewButton.setBounds(248, 359, 107, 23);
 		contentPane.add(btnNewButton);
 		
 		JButton btnNewButton_1 = new JButton("Cancelar");
-		btnNewButton_1.setBounds(215, 407, 89, 23);
+		btnNewButton_1.setBounds(149, 359, 89, 23);
 		contentPane.add(btnNewButton_1);
 		
 		JLabel lblNewLabel_5 = new JLabel("Descripción de Habitaciones: ");
-		lblNewLabel_5.setBounds(23, 240, 161, 14);
+		lblNewLabel_5.setBounds(23, 227, 161, 14);
 		contentPane.add(lblNewLabel_5);
 		
 		JLabel lblNewLabel_6 = new JLabel("Monto Total:");
-		lblNewLabel_6.setBounds(23, 352, 112, 14);
+		lblNewLabel_6.setBounds(23, 329, 112, 14);
 		contentPane.add(lblNewLabel_6);
+		
+		JLabel lblNewLabel_7 = new JLabel("DNI Del Titular de la Tarjeta");
+		lblNewLabel_7.setBounds(23, 171, 181, 14);
+		contentPane.add(lblNewLabel_7);
+		
+		textField_4 = new JTextField();
+		textField_4.setBounds(23, 187, 181, 20);
+		contentPane.add(textField_4);
+		textField_4.setColumns(10);
 	}
 }
