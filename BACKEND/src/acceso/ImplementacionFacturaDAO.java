dd9180f39f69a10fd71a8108b595b39eb5419f21
andressssm <andressanmartin894@gmail.com>
2024-10-21 16:21:47 -0300

    implementacion dao

---
+++ BACKEND/src/acceso/ImplementacionFacturaDAO.java
@@ -0,0 +1,40 @@
+package acceso;
+
+import java.util.Set;
+
+import ar.edu.unrn.seminario.api.FacturaDAO;
+import ar.edu.unrn.seminario.modelo.Factura;
+
+public class ImplementacionFacturaDAO implements FacturaDAO {
+
+	@Override
+	public void create(Factura factura) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void update(Factura factura) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Factura find(String nombre) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void remove(String nombre) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Set<Factura> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ BACKEND/src/acceso/ImplementaionServicioDAO.java
@@ -0,0 +1,40 @@
+package acceso;
+
+import java.util.Set;
+
+import ar.edu.unrn.seminario.api.ServicioDAO;
+import ar.edu.unrn.seminario.modelo.Servicio;
+
+public class ImplementaionServicioDAO implements ServicioDAO {
+
+	@Override
+	public void create(Servicio servicio) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void update(Servicio servicio) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Servicio find(String nombre) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void remove(String nombre) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Set<Servicio> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
