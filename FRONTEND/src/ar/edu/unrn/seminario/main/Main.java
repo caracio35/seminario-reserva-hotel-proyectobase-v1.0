a115861a547294325d8e4e35cbdfa835a0f7b504
caracio35 <128204576+caracio35@users.noreply.github.com>
2024-10-24 18:51:33 -0300

    main

--- FRONTEND/src/ar/edu/unrn/seminario/main/Main.java
+++ FRONTEND/src/ar/edu/unrn/seminario/main/Main.java
@@ -3,6 +3,8 @@ package ar.edu.unrn.seminario.main;
 import java.awt.EventQueue;
 
 import ar.edu.unrn.seminario.api.PersistenceApi;
+import ar.edu.unrn.seminario.gui.VentanaPrincipal;
+import ar.edu.unrn.seminario.api.*;
 
 public class Main {
 
@@ -12,17 +14,16 @@ public class Main {
 			public void run() {
 				try {
 
-					// IApi api = new MemoryApi();
-					// VentanaPrincipal frame = new VentanaPrincipal(api);
-					// frame.setVisible(true);
-					PersistenceApi p = new PersistenceApi();
-					//p.crearCaracteristicaEspecial("enano mimoso", "Expeciencia unica", 1000);
+					IApi api = new MemoryApi();
+					VentanaPrincipal frame = new VentanaPrincipal(api);
+					frame.setVisible(true);
+					// PersistenceApi p = new PersistenceApi();
+					// p.crearCaracteristicaEspecial("enano mimoso", "Expeciencia unica", 1000);
 					// p.eliminarCaracteristica("enano mimoso");
-					//p.obtenerCaracteristica();
-					//String [] car= {"Balcon","Pileta"};
-					//p.darDeAltaHabitacion(2, "LAAAAAA", 25000, true, 122, car);
-					
-					
+					// p.obtenerCaracteristica();
+					// String [] car= {"Balcon","Pileta"};
+					// p.darDeAltaHabitacion(2, "LAAAAAA", 25000, true, 122, car);
+
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
