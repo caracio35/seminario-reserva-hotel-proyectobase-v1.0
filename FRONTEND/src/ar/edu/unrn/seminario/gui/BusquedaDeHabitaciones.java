7cd783700bf4744a5c31c83f507e52e56405cb4e
andressssm <andressanmartin894@gmail.com>
2024-10-30 15:48:07 -0300

    ventana reserva

--- FRONTEND/src/ar/edu/unrn/seminario/gui/BusquedaDeHabitaciones.java
+++ FRONTEND/src/ar/edu/unrn/seminario/gui/BusquedaDeHabitaciones.java
@@ -43,7 +43,8 @@ public class BusquedaDeHabitaciones extends JFrame {
 	private DefaultTableModel modelo;
 	private IApi api;
 	private String fechaReservaFin;
-	private String fechaReservaInicio ; 
+	private String fechaReservaInicio;
+	private JTextField textFieldPrecio;
 
 	/**
 	 * Create the frame.
@@ -51,6 +52,7 @@ public class BusquedaDeHabitaciones extends JFrame {
 	@SuppressWarnings("serial")
 	public BusquedaDeHabitaciones(IApi api) {
 		this.api = api;
+
 		// Configurar el JFrame
 		setTitle("Búsqueda de Habitaciones");
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
@@ -65,7 +67,8 @@ public class BusquedaDeHabitaciones extends JFrame {
 		scrollPane.setBounds(136, 53, 815, 277);
 		getContentPane().add(scrollPane);
 
-		modelo = new DefaultTableModel(new Object[][] {}, new String[] { "Camas", "Descripcion", "Precio", "Numero de Habitacion", "  Caracteristicas Especiales   " }) {
+		modelo = new DefaultTableModel(new Object[][] {}, new String[] { "Camas", "Descripcion", "Precio",
+				"Numero de Habitacion", "  Caracteristicas Especiales   " }) {
 			public boolean isCellEditable(int row, int column) {
 				return false;
 			}
@@ -124,14 +127,10 @@ public class BusquedaDeHabitaciones extends JFrame {
 		comboBox.setBounds(10, 120, 96, 19);
 		getContentPane().add(comboBox);
 
-		JComboBox<String> comboBox_2 = new JComboBox<>();
-		comboBox_2.setBounds(10, 161, 96, 19);
-		getContentPane().add(comboBox_2);
-
 		JButton btnReservar = new JButton("RESERVAR");
 		btnReservar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				ConfirmarReserva confirmacion = new ConfirmarReserva( fechaReservaInicio,fechaReservaFin , null );
+				ConfirmarReserva confirmacion = new ConfirmarReserva(fechaReservaInicio, fechaReservaFin, null);
 				confirmacion.setVisible(true);
 			}
 		});
@@ -153,45 +152,54 @@ public class BusquedaDeHabitaciones extends JFrame {
 		getContentPane().add(btnCancelarsalir);
 
 		JDateChooser JcalenderFechaIngreso = new JDateChooser();
-        Calendar cal = Calendar.getInstance();
-        cal.add(Calendar.DAY_OF_MONTH, 0);
-        JcalenderFechaIngreso.setMinSelectableDate(cal.getTime());
+		Calendar cal = Calendar.getInstance();
+		cal.add(Calendar.DAY_OF_MONTH, 0);
+		JcalenderFechaIngreso.setMinSelectableDate(cal.getTime());
 		JcalenderFechaIngreso.getDateEditor().addPropertyChangeListener("date", new PropertyChangeListener() {
-            @Override
-            public void propertyChange(PropertyChangeEvent evt) {
-            	
-                Date selectedDate = JcalenderFechaIngreso.getDate();
-                if (selectedDate != null) {
-                    SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-                    fechaReservaInicio = sdf.format(selectedDate);
-                    
-                    System.out.println("Fecha seleccionada: " + fechaReservaFin);
-                } 
-            }
-        });
+			@Override
+			public void propertyChange(PropertyChangeEvent evt) {
+
+				Date selectedDate = JcalenderFechaIngreso.getDate();
+				if (selectedDate != null) {
+					SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+					fechaReservaInicio = sdf.format(selectedDate);
+
+					System.out.println("Fecha seleccionada: " + fechaReservaFin);
+				}
+			}
+		});
 		JcalenderFechaIngreso.setBounds(10, 204, 96, 20);
 		getContentPane().add(JcalenderFechaIngreso);
 
 		JDateChooser dateChooser_1 = new JDateChooser();
 		Calendar caleder = Calendar.getInstance();
-        caleder.add(Calendar.DAY_OF_MONTH, 0);
-        JcalenderFechaIngreso.setMinSelectableDate(caleder.getTime());
-        dateChooser_1.getDateEditor().addPropertyChangeListener("date", new PropertyChangeListener() {
-            @Override
-            public void propertyChange(PropertyChangeEvent evt) {
-                
-                Date selectedDate = dateChooser_1.getDate();
-                if (selectedDate != null) {
-   
-                    SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-                    fechaReservaFin = sdf.format(selectedDate);
-                    
-                    System.out.println("Fecha seleccionada: " + fechaReservaFin);
-                }
-            }
-        });
+		caleder.add(Calendar.DAY_OF_MONTH, 0);
+		JcalenderFechaIngreso.setMinSelectableDate(caleder.getTime());
+		dateChooser_1.getDateEditor().addPropertyChangeListener("date", new PropertyChangeListener() {
+			@Override
+			public void propertyChange(PropertyChangeEvent evt) {
+
+				Date selectedDate = dateChooser_1.getDate();
+				if (selectedDate != null) {
+
+					SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+					fechaReservaFin = sdf.format(selectedDate);
+
+					System.out.println("Fecha seleccionada: " + fechaReservaFin);
+				}
+			}
+		});
 		dateChooser_1.setBounds(10, 249, 96, 20);
 		getContentPane().add(dateChooser_1);
+
+		textFieldPrecio = new JTextField();
+		textFieldPrecio.setBounds(10, 163, 96, 19);
+		getContentPane().add(textFieldPrecio);
+		textFieldPrecio.setColumns(10);
+
+		JButton btnFiltrar = new JButton("filtrar");
+		btnFiltrar.setBounds(10, 322, 85, 21);
+		getContentPane().add(btnFiltrar);
 	}
 
 	public void cargarHabitaciones() {
--- FRONTEND/src/ar/edu/unrn/seminario/main/Main.java
+++ FRONTEND/src/ar/edu/unrn/seminario/main/Main.java
@@ -1,7 +1,6 @@
 package ar.edu.unrn.seminario.main;
 
 import java.awt.EventQueue;
-import java.util.ArrayList;
 import java.util.List;
 
 import ar.edu.unrn.seminario.api.IApi;
@@ -18,19 +17,19 @@ public class Main {
 			public void run() {
 				try {
 
-					//IApi api = new MemoryApi();
-					//VentanaPrincipal frame = new VentanaPrincipal(api);
-					//frame.setVisible(true);
+					IApi api = new MemoryApi();
+					VentanaPrincipal frame = new VentanaPrincipal(api);
+					frame.setVisible(true);
 					PersistenceApi p = new PersistenceApi();
-					//p.crearCaracteristicaEspecial("enano mimoso", "Expeciencia unica", 1000);
-					//p.eliminarCaracteristica("enano mimoso");
-					//p.obtenerCaracteristica();
-					//String[] car = {"buee"};
-					//p.darDeAltaHabitacion(2, "LAAAAAA", 25000, true, 22, car);
+					// p.crearCaracteristicaEspecial("enano mimoso", "Expeciencia unica", 1000);
+					// p.eliminarCaracteristica("enano mimoso");
+					// p.obtenerCaracteristica();
+					// String[] car = {"buee"};
+					// p.darDeAltaHabitacion(2, "LAAAAAA", 25000, true, 22, car);
 					List<HabitacionDTO> ha = p.obtenerTodasLasHabitaciones();
 					for (HabitacionDTO h : ha) {
 						System.out.println(h.getNumHabitacion());
-						
+
 					}
 				} catch (Exception e) {
 					e.printStackTrace();
