f3307d42e70a65f0656ebe191a0a37b186d3ef21
caracio35 <128204576+caracio35@users.noreply.github.com>
2024-11-10 12:28:14 -0300

    Refactor ImplementacionReservaDAO methods for better readability; add exception handling and additional service and room data retrieval

--- BACKEND/src/acceso/ImplementacionReservaDAO.java
+++ BACKEND/src/acceso/ImplementacionReservaDAO.java
@@ -5,81 +5,106 @@ import java.sql.Date;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
 import com.mysql.jdbc.PreparedStatement;
 import com.mysql.jdbc.Statement;
 import ar.edu.unrn.seminario.api.ReservaDAO;
+import ar.edu.unrn.seminario.exception.CampoVacioExeption;
 import ar.edu.unrn.seminario.exception.ConexionFallidaExeption;
+import ar.edu.unrn.seminario.exception.EnterosEnCeroExeption;
+import ar.edu.unrn.seminario.exception.PrecioCeroExeption;
+import ar.edu.unrn.seminario.modelo.CaracteristicaEspecial;
 import ar.edu.unrn.seminario.modelo.Habitacion;
 import ar.edu.unrn.seminario.modelo.Reserva;
 import ar.edu.unrn.seminario.modelo.Servicio;
 
-public class ImplementacionReservaDAO implements ReservaDAO{
+public class ImplementacionReservaDAO implements ReservaDAO {
 	private final static String conexion = "jdbc:mysql://localhost:3306/Comarca Hoteles?useSSL=false";
 	private final static String usuario = "root";
 	private final static String clave = "";
 	private final static String crearHabitacion = "INSERT INTO reserva (usuario_id, fechaDeInicio, fechaDeSalida, cantidadDePersonas,"
-			+ "                      fechaDeReserva, saldoFavor, pagoMinimo) VALUES (?, ?, ?, ?, ?, ?, ?);" ;  
-	
+			+ "                      fechaDeReserva, saldoFavor, pagoMinimo) VALUES (?, ?, ?, ?, ?, ?, ?);";
+
+	private static final String SELECT_ALL_RESERVAS = "SELECT * FROM Reserva";
+	private static final String SELECT_HABITACIONES_BY_RESERVA = "SELECT h.numHabitaciones, h.cantidadDeCamas, h.descripcion, h.precio, h.habilitado "
+			+
+			"FROM Habitacion h " +
+			"JOIN Reserva_Habitacion rh ON h.numHabitaciones = rh.numHabitacion " +
+			"WHERE rh.reserva_id = ?";
+	private static final String SELECT_SERVICIOS_BY_RESERVA = "SELECT s.id, s.nombre, s.precio, s.descripcion " +
+			"FROM Servicio s " +
+			"JOIN Reserva_Servicio rs ON s.id = rs.servicio_id " +
+			"WHERE rs.reserva_id = ?";
+	private static final String SELECT_CARACTERISTICAS_BY_HABITACION = "SELECT c.nombre, c.descripcion, c.precio " +
+			"FROM CaracteristicaEspecial c " +
+			"JOIN Habitacion_CaracteristicaEspecial hc ON c.nombre = hc.nombreCaracteristicaEspecial " +
+			"WHERE hc.numHabitacion = ?";
+
 	@Override
 	public void create(Reserva reserva) {
 		Connection miConeccion = null;
-	    PreparedStatement pStamentConsutaCreaReserva = null;
-	    try {
-	    miConeccion = conectar();
-        miConeccion.setAutoCommit(false);
-        pStamentConsutaCreaReserva = (PreparedStatement) miConeccion.prepareStatement(crearHabitacion, Statement.RETURN_GENERATED_KEYS);
-        
-        Optional<Integer> obtenerlUserId = findUserId(reserva, miConeccion);
-        int idUsuario = obtenerlUserId.get();
-        
-        pStamentConsutaCreaReserva.setInt(1, idUsuario);
-        pStamentConsutaCreaReserva.setDate(2, Date.valueOf(reserva.getFechaDeInicio()));
-        pStamentConsutaCreaReserva.setDate(3, Date.valueOf(reserva.getFechaDESalida()));
-        pStamentConsutaCreaReserva.setInt(4, reserva.getCantidadDePersonas());
-        pStamentConsutaCreaReserva.setDate(5, Date.valueOf(reserva.getFechaDeReserva()));
-        pStamentConsutaCreaReserva.setDouble(6, reserva.getSaldofavor());
-        pStamentConsutaCreaReserva.setBoolean(7, reserva.getPagoMinimo());
-        
-        pStamentConsutaCreaReserva.executeUpdate();
-        
-        ResultSet obtenerIdReserva = pStamentConsutaCreaReserva.getGeneratedKeys();
-        int reservaId = 0; 
-        if (obtenerIdReserva.next()) { 
-            reservaId = obtenerIdReserva.getInt(1); 
-        }
-        insertRooms(reservaId, reserva, miConeccion);
-        insertServices(reservaId, reserva, miConeccion);
-        miConeccion.commit();
-        
-        } catch (SQLException e) {
-	        try {
-	            if (miConeccion != null) {
-	                miConeccion.rollback(); 
-	                System.out.println("hizo rolsasfd");
-	            }
-	        } catch (SQLException ex) {
-	            System.out.println("Error al hacer rollback");
-	            ex.printStackTrace();
-	        }
-	        e.printStackTrace();
-	    } finally {
-	        try {
-	            if (pStamentConsutaCreaReserva != null) pStamentConsutaCreaReserva.close();
-	            if (miConeccion != null) miConeccion.close();
-	        } catch (SQLException e) {
-	            System.out.println("Error al cerrar la conexión");
-	        }
-	    }
-	}
+		PreparedStatement pStamentConsutaCreaReserva = null;
+		try {
+			miConeccion = conectar();
+			miConeccion.setAutoCommit(false);
+			pStamentConsutaCreaReserva = (PreparedStatement) miConeccion.prepareStatement(crearHabitacion,
+					Statement.RETURN_GENERATED_KEYS);
 
+			Optional<Integer> obtenerlUserId = findUserId(reserva, miConeccion);
+			int idUsuario = obtenerlUserId.get();
+
+			pStamentConsutaCreaReserva.setInt(1, idUsuario);
+			pStamentConsutaCreaReserva.setDate(2, Date.valueOf(reserva.getFechaDeInicio()));
+			pStamentConsutaCreaReserva.setDate(3, Date.valueOf(reserva.getFechaDESalida()));
+			pStamentConsutaCreaReserva.setInt(4, reserva.getCantidadDePersonas());
+			pStamentConsutaCreaReserva.setDate(5, Date.valueOf(reserva.getFechaDeReserva()));
+			pStamentConsutaCreaReserva.setDouble(6, reserva.getSaldofavor());
+			pStamentConsutaCreaReserva.setBoolean(7, reserva.getPagoMinimo());
+
+			pStamentConsutaCreaReserva.executeUpdate();
+
+			ResultSet obtenerIdReserva = pStamentConsutaCreaReserva.getGeneratedKeys();
+			int reservaId = 0;
+			if (obtenerIdReserva.next()) {
+				reservaId = obtenerIdReserva.getInt(1);
+			}
+			insertRooms(reservaId, reserva, miConeccion);
+			insertServices(reservaId, reserva, miConeccion);
+			miConeccion.commit();
+
+		} catch (SQLException e) {
+			try {
+				if (miConeccion != null) {
+					miConeccion.rollback();
+					System.out.println("hizo rolsasfd");
+				}
+			} catch (SQLException ex) {
+				System.out.println("Error al hacer rollback");
+				ex.printStackTrace();
+			}
+			e.printStackTrace();
+		} finally {
+			try {
+				if (pStamentConsutaCreaReserva != null)
+					pStamentConsutaCreaReserva.close();
+				if (miConeccion != null)
+					miConeccion.close();
+			} catch (SQLException e) {
+				System.out.println("Error al cerrar la conexión");
+			}
+		}
+	}
 
 	@Override
 	public void update(Reserva reserva) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
@@ -91,92 +116,197 @@ public class ImplementacionReservaDAO implements ReservaDAO{
 	@Override
 	public void remove(String nombre) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
-	public Set<Reserva> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+	public Set<Reserva> findAll() throws CampoVacioExeption, EnterosEnCeroExeption, PrecioCeroExeption {
+		Set<Reserva> reservas = new HashSet<>();
+
+		try (Connection conn = conectar();
+				java.sql.PreparedStatement stmtReservas = conn.prepareStatement(SELECT_ALL_RESERVAS);
+				ResultSet rsReservas = stmtReservas.executeQuery()) {
+
+			while (rsReservas.next()) {
+				// Obtener datos básicos de la reserva
+				int reservaId = rsReservas.getInt("id");
+				LocalDate fechaDeInicio = rsReservas.getDate("fechaDeInicio").toLocalDate();
+				LocalDate fechaDeSalida = rsReservas.getDate("fechaDeSalida").toLocalDate();
+				int cantidadDePersonas = rsReservas.getInt("cantidadDePersonas");
+				LocalDate fechaDeReserva = rsReservas.getDate("fechaDeReserva").toLocalDate();
+				boolean pagoMinimo = rsReservas.getBoolean("pagoMinimo");
+
+				// Crear la lista de habitaciones asociadas a la reserva
+				ArrayList<Habitacion> habitaciones = obtenerHabitacionesPorReserva(conn, reservaId);
+
+				// Crear la lista de servicios asociados a la reserva
+				ArrayList<Servicio> servicios = obtenerServiciosPorReserva(conn, reservaId);
+
+				// Crear la reserva y agregarla al conjunto
+				Reserva reserva = new Reserva(reservaId, habitaciones, null, fechaDeInicio, fechaDeSalida,
+						cantidadDePersonas, servicios, fechaDeReserva, pagoMinimo);
+				reservas.add(reserva);
+			}
+
+		} catch (SQLException e) {
+			System.out.println("Error al obtener todas las reservas: " + e.getMessage());
+			e.printStackTrace();
+		}
+
+		return reservas;
 	}
+
+	private ArrayList<Habitacion> obtenerHabitacionesPorReserva(Connection conn, int reservaId)
+			throws SQLException, CampoVacioExeption, EnterosEnCeroExeption, PrecioCeroExeption {
+		ArrayList<Habitacion> habitaciones = new ArrayList<>();
+		try (java.sql.PreparedStatement stmtHabitaciones = conn.prepareStatement(SELECT_HABITACIONES_BY_RESERVA)) {
+			stmtHabitaciones.setInt(1, reservaId);
+			ResultSet rsHabitaciones = stmtHabitaciones.executeQuery();
+
+			while (rsHabitaciones.next()) {
+				int numHabitaciones = rsHabitaciones.getInt("numHabitaciones");
+				int cantidadDeCamas = rsHabitaciones.getInt("cantidadDeCamas");
+				String descripcion = rsHabitaciones.getString("descripcion");
+				double precio = rsHabitaciones.getDouble("precio");
+				boolean habilitado = rsHabitaciones.getBoolean("habilitado");
+
+				// Obtener características especiales de la habitación
+				ArrayList<CaracteristicaEspecial> caracteristicas = obtenerCaracteristicasPorHabitacion(conn,
+						numHabitaciones);
+
+				Habitacion habitacion = new Habitacion(cantidadDeCamas, descripcion, precio, habilitado,
+						numHabitaciones, caracteristicas);
+				habitaciones.add(habitacion);
+			}
+		}
+		return habitaciones;
+	}
+
+	private ArrayList<CaracteristicaEspecial> obtenerCaracteristicasPorHabitacion(Connection conn, int numHabitacion)
+			throws SQLException {
+		ArrayList<CaracteristicaEspecial> caracteristicas = new ArrayList<>();
+		try (java.sql.PreparedStatement stmtCaracteristicas = conn
+				.prepareStatement(SELECT_CARACTERISTICAS_BY_HABITACION)) {
+			stmtCaracteristicas.setInt(1, numHabitacion);
+			ResultSet rsCaracteristicas = stmtCaracteristicas.executeQuery();
+
+			while (rsCaracteristicas.next()) {
+				String nombre = rsCaracteristicas.getString("nombre");
+				String descripcion = rsCaracteristicas.getString("descripcion");
+				double precio = rsCaracteristicas.getDouble("precio");
+
+				CaracteristicaEspecial caracteristica = new CaracteristicaEspecial(nombre, descripcion, precio);
+				caracteristicas.add(caracteristica);
+			}
+		}
+		return caracteristicas;
+	}
+
+	private ArrayList<Servicio> obtenerServiciosPorReserva(Connection conn, int reservaId) throws SQLException {
+		ArrayList<Servicio> servicios = new ArrayList<>();
+		try (java.sql.PreparedStatement stmtServicios = conn.prepareStatement(SELECT_SERVICIOS_BY_RESERVA)) {
+			stmtServicios.setInt(1, reservaId);
+			ResultSet rsServicios = stmtServicios.executeQuery();
+
+			while (rsServicios.next()) {
+				int servicioId = rsServicios.getInt("id");
+				String nombre = rsServicios.getString("nombre");
+				double precio = rsServicios.getDouble("precio");
+				String descripcion = rsServicios.getString("descripcion");
+
+				Servicio servicio = new Servicio(servicioId, nombre, precio, descripcion);
+				servicios.add(servicio);
+			}
+		}
+		return servicios;
+	}
+
 	private Optional<Integer> findUserId(Reserva r, Connection miConeccion) throws SQLException {
 		String buscarIdUsuario = "SELECT id FROM usuarios WHERE nombre = ? AND email = ?";
 		PreparedStatement pStamentUsuario = (PreparedStatement) miConeccion.prepareStatement(buscarIdUsuario);
 		pStamentUsuario.setString(1, r.getUsuario().getNombre());
-	    pStamentUsuario.setString(2, r.getUsuario().getEmail());
-	    
-	    ResultSet rs = pStamentUsuario.executeQuery();
-	    if (rs.next()) {
-	        return Optional.of(rs.getInt("id"));
-	    }
-	    return Optional.empty(); 
+		pStamentUsuario.setString(2, r.getUsuario().getEmail());
+
+		ResultSet rs = pStamentUsuario.executeQuery();
+		if (rs.next()) {
+			return Optional.of(rs.getInt("id"));
+		}
+		return Optional.empty();
 	}
 
-	private void insertRooms(int reservaId , Reserva r ,Connection miConeccion) {
-		
-		 PreparedStatement pStamentBuscarHabitacion = null;
-		    PreparedStatement pStamentConsutaInsertaHabitacion = null;
-		    ResultSet rsHabitacion = null;
-		    try { 
-		    	for (Habitacion h : r.getHabitacion()) 
-		    	{ 
-		    		String buscarHabitacion = "SELECT * FROM habitacion WHERE numHabitaciones = ?";
-		    		pStamentBuscarHabitacion = (PreparedStatement) miConeccion.prepareStatement(buscarHabitacion);
-		    		pStamentBuscarHabitacion.setInt(1, h.getNumHabitaciones());
-		    		rsHabitacion = pStamentBuscarHabitacion.executeQuery();
-		    		if (rsHabitacion.next())
-		    		{
-		    			String insertarReservaHabitacion = "INSERT INTO reserva_habitacion (reserva_id, numHabitacion) VALUES (?, ?)";
-		    			pStamentConsutaInsertaHabitacion = (PreparedStatement) miConeccion.prepareStatement(insertarReservaHabitacion);
-		    			pStamentConsutaInsertaHabitacion.setInt(1, reservaId);
-		    			pStamentConsutaInsertaHabitacion.setInt(2, rsHabitacion.getInt("numHabitaciones")); 
-		    			pStamentConsutaInsertaHabitacion.executeUpdate();
-                }
-            }
-		}catch (SQLException e) {
-        e.printStackTrace();
-        } finally {
-        try {
-            if (rsHabitacion != null) rsHabitacion.close();
-            if (pStamentBuscarHabitacion != null) pStamentBuscarHabitacion.close();
-            if (pStamentConsutaInsertaHabitacion != null) pStamentConsutaInsertaHabitacion.close();
-        } catch (SQLException e) {
-            System.out.println("Error al cerrar los recursos");
-        }
-    }
+	private void insertRooms(int reservaId, Reserva r, Connection miConeccion) {
+
+		PreparedStatement pStamentBuscarHabitacion = null;
+		PreparedStatement pStamentConsutaInsertaHabitacion = null;
+		ResultSet rsHabitacion = null;
+		try {
+			for (Habitacion h : r.getHabitacion()) {
+				String buscarHabitacion = "SELECT * FROM habitacion WHERE numHabitaciones = ?";
+				pStamentBuscarHabitacion = (PreparedStatement) miConeccion.prepareStatement(buscarHabitacion);
+				pStamentBuscarHabitacion.setInt(1, h.getNumHabitaciones());
+				rsHabitacion = pStamentBuscarHabitacion.executeQuery();
+				if (rsHabitacion.next()) {
+					String insertarReservaHabitacion = "INSERT INTO reserva_habitacion (reserva_id, numHabitacion) VALUES (?, ?)";
+					pStamentConsutaInsertaHabitacion = (PreparedStatement) miConeccion
+							.prepareStatement(insertarReservaHabitacion);
+					pStamentConsutaInsertaHabitacion.setInt(1, reservaId);
+					pStamentConsutaInsertaHabitacion.setInt(2, rsHabitacion.getInt("numHabitaciones"));
+					pStamentConsutaInsertaHabitacion.executeUpdate();
+				}
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (rsHabitacion != null)
+					rsHabitacion.close();
+				if (pStamentBuscarHabitacion != null)
+					pStamentBuscarHabitacion.close();
+				if (pStamentConsutaInsertaHabitacion != null)
+					pStamentConsutaInsertaHabitacion.close();
+			} catch (SQLException e) {
+				System.out.println("Error al cerrar los recursos");
+			}
+		}
 	}
+
 	private void insertServices(int reservaId, Reserva r, Connection miConeccion) {
-	    PreparedStatement pStamentBuscarServicio = null;
-	    PreparedStatement pStamentConsutaInsertaServicio = null;
-	    ResultSet rsServicio = null;
+		PreparedStatement pStamentBuscarServicio = null;
+		PreparedStatement pStamentConsutaInsertaServicio = null;
+		ResultSet rsServicio = null;
 
-	    try {
-	        for (Servicio s : r.getServicios()) { 
-	        	System.out.println(s.getNombre());
-	            String buscarServicio = "SELECT * FROM servicio WHERE nombre = ?";
-	            pStamentBuscarServicio = (PreparedStatement) miConeccion.prepareStatement(buscarServicio);
-	            pStamentBuscarServicio.setString(1,s.getNombre()); 
-	            rsServicio = pStamentBuscarServicio.executeQuery();
-	            if (rsServicio.next()) {
-	                String insertarReservaServicio = "INSERT INTO reserva_servicio (reserva_id, servicio_id) VALUES (?, ?)";
-	                pStamentConsutaInsertaServicio = (PreparedStatement) miConeccion.prepareStatement(insertarReservaServicio);
-	                pStamentConsutaInsertaServicio.setInt(1, reservaId);
-	                pStamentConsutaInsertaServicio.setInt(2, rsServicio.getInt("id")); 
-	                pStamentConsutaInsertaServicio.executeUpdate();
-	            }
-	        }
-	    } catch (SQLException e) {
-	        e.printStackTrace(); 
-	    } finally {
-	        try {
-	            if (rsServicio != null) rsServicio.close();
-	            if (pStamentBuscarServicio != null) pStamentBuscarServicio.close();
-	            if (pStamentConsutaInsertaServicio != null) pStamentConsutaInsertaServicio.close();
-	        } catch (SQLException e) {
-	            System.out.println("Error al cerrar los recursos");
-	        }
-	    }
+		try {
+			for (Servicio s : r.getServicios()) {
+				System.out.println(s.getNombre());
+				String buscarServicio = "SELECT * FROM servicio WHERE nombre = ?";
+				pStamentBuscarServicio = (PreparedStatement) miConeccion.prepareStatement(buscarServicio);
+				pStamentBuscarServicio.setString(1, s.getNombre());
+				rsServicio = pStamentBuscarServicio.executeQuery();
+				if (rsServicio.next()) {
+					String insertarReservaServicio = "INSERT INTO reserva_servicio (reserva_id, servicio_id) VALUES (?, ?)";
+					pStamentConsutaInsertaServicio = (PreparedStatement) miConeccion
+							.prepareStatement(insertarReservaServicio);
+					pStamentConsutaInsertaServicio.setInt(1, reservaId);
+					pStamentConsutaInsertaServicio.setInt(2, rsServicio.getInt("id"));
+					pStamentConsutaInsertaServicio.executeUpdate();
+				}
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (rsServicio != null)
+					rsServicio.close();
+				if (pStamentBuscarServicio != null)
+					pStamentBuscarServicio.close();
+				if (pStamentConsutaInsertaServicio != null)
+					pStamentConsutaInsertaServicio.close();
+			} catch (SQLException e) {
+				System.out.println("Error al cerrar los recursos");
+			}
+		}
 	}
+
 	private Connection conectar() throws ConexionFallidaExeption {
 		Connection miConnecion = null;
 		try {
@@ -187,6 +317,5 @@ public class ImplementacionReservaDAO implements ReservaDAO{
 
 		}
 	}
-
 
 }
--- BACKEND/src/ar/edu/unrn/seminario/api/ReservaDAO.java
+++ BACKEND/src/ar/edu/unrn/seminario/api/ReservaDAO.java
@@ -3,14 +3,20 @@ package ar.edu.unrn.seminario.api;
 import java.util.Optional;
 import java.util.Set;
 
-
+import ar.edu.unrn.seminario.exception.CampoVacioExeption;
+import ar.edu.unrn.seminario.exception.EnterosEnCeroExeption;
+import ar.edu.unrn.seminario.exception.PrecioCeroExeption;
 import ar.edu.unrn.seminario.modelo.Reserva;
 
 public interface ReservaDAO {
-	
-	void create (Reserva reserva);
+
+	void create(Reserva reserva);
+
 	void update(Reserva reserva);
+
 	Optional<Reserva> find(int idReserva);
+
 	void remove(String nombre);
-	Set<Reserva>findAll();
+
+	Set<Reserva> findAll() throws CampoVacioExeption, EnterosEnCeroExeption, PrecioCeroExeption;
 }
