935452e20bd2f66b28258fe0aa1613be333b8e6c
andressssm <andressanmartin894@gmail.com>
2024-12-16 17:20:28 -0300

    ventana principal

--- FRONTEND/src/ar/edu/unrn/seminario/gui/VentanaPrincipal.java
+++ FRONTEND/src/ar/edu/unrn/seminario/gui/VentanaPrincipal.java
@@ -1,7 +1,9 @@
 package ar.edu.unrn.seminario.gui;
+
 import java.awt.BorderLayout;
 import java.util.Locale;
 import java.util.ResourceBundle;
+
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
@@ -9,6 +11,7 @@ import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+
 import ar.edu.unrn.seminario.api.IApi;
 import ar.edu.unrn.seminario.exception.CampoVacioExeption;
 import ar.edu.unrn.seminario.exception.ConexionFallidaExeption;
@@ -17,89 +20,91 @@ import ar.edu.unrn.seminario.exception.PrecioCeroExeption;
 
 public class VentanaPrincipal extends JFrame {
 
-    private JPanel contentPane;
-    public VentanaPrincipal(IApi api, Locale seleccion) {
+	private JPanel contentPane;
 
-        ResourceBundle labels = ResourceBundle.getBundle("labels", seleccion);
+	public VentanaPrincipal(IApi api, Locale seleccion) {
 
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setBounds(100, 100, 450, 300);
+		ResourceBundle labels = ResourceBundle.getBundle("labels", seleccion);
 
-        JMenuBar menuBar = new JMenuBar();
-        setJMenuBar(menuBar);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
 
-        JMenu mnReservas = new JMenu(labels.getString("menu.reservas"));
-        menuBar.add(mnReservas);
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
 
-        JMenuItem mntmBuscarHabitacion = new JMenuItem(labels.getString("menuitem.buscar_habitacion"));
-        mntmBuscarHabitacion.addActionListener(e -> {
-            BusquedaDeHabitaciones busqueda = new BusquedaDeHabitaciones(api, seleccion);
-            busqueda.setVisible(true);
-        });
-        mnReservas.add(mntmBuscarHabitacion);
+		JMenu mnReservas = new JMenu(labels.getString("menu.reservas"));
+		menuBar.add(mnReservas);
 
-        JMenuItem mntmMisReservas = new JMenuItem(labels.getString("menuitem.mis_reservas"));
-        mntmMisReservas.addActionListener(e -> {
-            try {
-                VerReservas misReservas = new VerReservas(api, seleccion);
-                misReservas.setVisible(true);
-            } catch (CampoVacioExeption | EnterosEnCeroExeption | PrecioCeroExeption e1) {
-                JOptionPane.showMessageDialog(null, e1.getMessage());
-            }
-        });
-        mnReservas.add(mntmMisReservas);
+		JMenuItem mntmBuscarHabitacion = new JMenuItem(labels.getString("menuitem.buscar_habitacion"));
+		mntmBuscarHabitacion.addActionListener(e -> {
+			BusquedaDeHabitaciones busqueda = new BusquedaDeHabitaciones(api, seleccion);
+			busqueda.setVisible(true);
+		});
+		mnReservas.add(mntmBuscarHabitacion);
 
-        JMenu usuarioMenu = new JMenu(labels.getString("menu.usuarios"));
-        usuarioMenu.setVisible(api.esAdmin());
-        menuBar.add(usuarioMenu);
+		JMenuItem mntmMisReservas = new JMenuItem(labels.getString("menuitem.mis_reservas"));
+		mntmMisReservas.addActionListener(e -> {
+			try {
+				VerReservas misReservas = new VerReservas(api, seleccion);
+				misReservas.setVisible(true);
+			} catch (CampoVacioExeption | EnterosEnCeroExeption | PrecioCeroExeption e1) {
+				JOptionPane.showMessageDialog(null, e1.getMessage());
+			}
+		});
+		mnReservas.add(mntmMisReservas);
 
-        JMenuItem altaUsuarioMenuItem = new JMenuItem(labels.getString("menuitem.alta_usuario"));
-        altaUsuarioMenuItem.addActionListener(arg0 -> {
-            AltaUsuario alta = new AltaUsuario(api);
-            alta.setLocationRelativeTo(null);
-            alta.setVisible(true);
-        });
-        usuarioMenu.add(altaUsuarioMenuItem);
+		JMenu usuarioMenu = new JMenu(labels.getString("menu.usuarios"));
+		usuarioMenu.setVisible(api.esAdmin());
+		menuBar.add(usuarioMenu);
 
-        JMenuItem listadoUsuarioMenuItem = new JMenuItem(labels.getString("menuitem.listado_usuario"));
-        listadoUsuarioMenuItem.addActionListener(arg0 -> {
-        	 JOptionPane.showMessageDialog(null, "se implementara el la proxima actualizacion");
-        });
-        usuarioMenu.add(listadoUsuarioMenuItem);
+		JMenuItem altaUsuarioMenuItem = new JMenuItem(labels.getString("menuitem.alta_usuario"));
+		altaUsuarioMenuItem.addActionListener(arg0 -> {
+			JOptionPane.showMessageDialog(null, "Se Implementara el la proxima actulizacion");
+//            AltaUsuario alta = new AltaUsuario(api);
+//            alta.setLocationRelativeTo(null);
+//            alta.setVisible(true);
+		});
+		usuarioMenu.add(altaUsuarioMenuItem);
 
-        JMenu mnHabitaciones = new JMenu(labels.getString("menu.habitaciones"));
-        mnHabitaciones.setVisible(api.esAdmin());
-        menuBar.add(mnHabitaciones);
+		JMenuItem listadoUsuarioMenuItem = new JMenuItem(labels.getString("menuitem.listado_usuario"));
+		listadoUsuarioMenuItem.addActionListener(arg0 -> {
+			JOptionPane.showMessageDialog(null, "se implementara el la proxima actualizacion");
+		});
+		usuarioMenu.add(listadoUsuarioMenuItem);
 
-        JMenuItem mntmCargarHabitacion = new JMenuItem(labels.getString("menuitem.cargar_habitacion"));
-        mntmCargarHabitacion.addActionListener(e -> {
-            CargarHabitacion cargaH = new CargarHabitacion(api, false, seleccion);
-            api.modificarFalse();
-            cargaH.setVisible(true);
-        });
-        mnHabitaciones.add(mntmCargarHabitacion);
+		JMenu mnHabitaciones = new JMenu(labels.getString("menu.habitaciones"));
+		mnHabitaciones.setVisible(api.esAdmin());
+		menuBar.add(mnHabitaciones);
 
-        JMenuItem mntmListadoHabitaciones = new JMenuItem(labels.getString("menuitem.listado_habitaciones"));
-        mntmListadoHabitaciones.addActionListener(e -> {
-            try {
-                ListadoHabitaciones listaHabitaciones = new ListadoHabitaciones(api, seleccion);
-                listaHabitaciones.setVisible(true);
-            } catch (ConexionFallidaExeption e1) {
-                JOptionPane.showMessageDialog(null, e1.getMessage());
-            }
-        });
-        mnHabitaciones.add(mntmListadoHabitaciones);
+		JMenuItem mntmCargarHabitacion = new JMenuItem(labels.getString("menuitem.cargar_habitacion"));
+		mntmCargarHabitacion.addActionListener(e -> {
+			CargarHabitacion cargaH = new CargarHabitacion(api, false, seleccion);
+			api.modificarFalse();
+			cargaH.setVisible(true);
+		});
+		mnHabitaciones.add(mntmCargarHabitacion);
 
-        JMenu configuracionMenu = new JMenu(labels.getString("menu.configuracion"));
-        menuBar.add(configuracionMenu);
+		JMenuItem mntmListadoHabitaciones = new JMenuItem(labels.getString("menuitem.listado_habitaciones"));
+		mntmListadoHabitaciones.addActionListener(e -> {
+			try {
+				ListadoHabitaciones listaHabitaciones = new ListadoHabitaciones(api, seleccion);
+				listaHabitaciones.setVisible(true);
+			} catch (ConexionFallidaExeption e1) {
+				JOptionPane.showMessageDialog(null, e1.getMessage());
+			}
+		});
+		mnHabitaciones.add(mntmListadoHabitaciones);
 
-        JMenuItem salirMenuItem = new JMenuItem(labels.getString("menuitem.salir"));
-        salirMenuItem.addActionListener(e -> VentanaPrincipal.this.dispose());
-        configuracionMenu.add(salirMenuItem);
+		JMenu configuracionMenu = new JMenu(labels.getString("menu.configuracion"));
+		menuBar.add(configuracionMenu);
 
-        contentPane = new JPanel();
-        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-        contentPane.setLayout(new BorderLayout(0, 0));
-        setContentPane(contentPane);
-    }
+		JMenuItem salirMenuItem = new JMenuItem(labels.getString("menuitem.salir"));
+		salirMenuItem.addActionListener(e -> VentanaPrincipal.this.dispose());
+		configuracionMenu.add(salirMenuItem);
+
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+	}
 }
